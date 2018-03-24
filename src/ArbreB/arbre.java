f0e4074ec7968bcc6367cb46389f1397ca2a763b
Samuel <samuel.lhote@etud.u-picardie.fr>
2018-03-24 11:59:01 +0100

    Premier Commit

---
+++ src/ArbreB/arbre.java
@@ -0,0 +1,29 @@
+package ArbreB;
+
+public class arbre {
+
+	noeud racine;
+	static int ordre;
+	
+	public arbre(int ordre) {
+		this.ordre = ordre;
+		racine = new noeud(ordre,null);		
+    }
+	
+	public void recherche(noeud racine, int c)
+    {       
+        int index = 0;
+       
+		while(i < racine.nombreclé && clé > racine.clé[i])
+		{
+			i++;
+		}
+		
+		
+		
+		
+		
+		
+    }
+
+}
